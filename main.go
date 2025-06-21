8941a21414c9b9abc25331be25ea67691b07314c
hexlunapng <hexishexomgcute@gmail.com>
2025-06-20 23:07:38 -0400

    ass

--- main.go
+++ main.go
@@ -8,3 +8,38 @@ import (
 "syscall"
 "github.com/bwmarrin/discordgo"
 )
+
+var (
+	token = flag.String("token", "", "Discord bot token")
+	GuildID = flag.String("guild", "", "Guild ID")
+)
+
+var bot *discordgo.Session
+
+
+func init() { flag.Parse ()}
+
+func init () { 
+	var err error
+	bot, err = discordgo.NewSession(token)
+	if err != nil {
+		fmt.Println(err)
+		return
+}
+
+var handler = func(s *discordgo.Session, m *discordgo.MessageCreate) {
+	}
+	func main() {
+		bot.AddHandler(handler)
+ bot.Open()
+ if err := bot.Identify(); err != nil {
+	fmt.Println(err)
+	return
+ }
+ defer bot.Close()
+ 
+ fmt.Println("bot is running")
+ stop := make(chan os.Signal, 1)
+ signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+ <-stop
+}
